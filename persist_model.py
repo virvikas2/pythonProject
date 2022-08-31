50e63d073e5dc53dd87b1dcc2cc09f734c14e8d7
Kumar, Vikas <Vikas.Kumar11@emc.com>
2022-08-31 11:16:36 +0530

    Adding files

--- persist_model.py
+++ persist_model.py
@@ -1,10 +1,13 @@
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
+# from sklearn.externals import joblib
+import joblib
 music_data = pd.read_csv('music.csv')
 input_data = music_data.drop(columns=['genre'])
 output_data = music_data['genre']
 
 model = DecisionTreeClassifier()
 model.fit(input_data.values, output_data)
+joblib.dump(model, 'music_persist_model.joblib')
 # predictions = model.predict([ [21, 1], [22, 0]])
 # print(predictions)
